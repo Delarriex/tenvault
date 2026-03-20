e4dfbb345b90fc1fb97d741a34cc75cd921f6be0
Web Admin <admin@localhost>
2026-03-20 09:08:11 -0700

    Initial commit

---
+++ contactus/my-account/assets/js/script.js
@@ -0,0 +1,25 @@
+
+//   // Disable right-click
+//   document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+//   });
+
+//   // Disable text selection
+//   document.onselectstart = function() { return false; };
+//   document.onmousedown = function() { return false; };
+
+//   // Disable common shortcuts: Ctrl+U, Ctrl+C, Ctrl+S, etc.
+//   document.addEventListener('keydown', function(e) {
+//     if ((e.ctrlKey && (e.keyCode === 85 || e.keyCode === 67 || e.keyCode === 83 || e.keyCode === 73))) {
+//       e.preventDefault();
+//     }
+//   });
+
+
+
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 85 || e.keyCode === 83 || e.keyCode === 73)) {
+//       e.preventDefault();
+//     }
+//   });
